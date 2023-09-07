90c11935c607b2c491fe3149a0b61df5fe22bd6c
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-07 13:52:10 +0900

    update atcoder template.kt

--- atcoder/template.kt
+++ atcoder/template.kt
@@ -1,9 +1,8 @@
 private fun readStr() = readLine()!! // string line
-
 private fun readInt() = readStr().toInt() // single int
-
+private fun readLong() = readStr().toLong() // single long
 private fun readStrings() = readStr().split(" ") // list of strings
-
 private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
 
 fun main() {}
