3f0ab2fec3173863896d9c7211a037711a9e78f6
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-07 10:06:36 +0900

    add template and setup script to atcoder

---
+++ atcoder/template.kt
@@ -0,0 +1,9 @@
+private fun readStr() = readLine()!! // string line
+
+private fun readInt() = readStr().toInt() // single int
+
+private fun readStrings() = readStr().split(" ") // list of strings
+
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+
+fun main() {}
