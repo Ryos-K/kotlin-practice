0a643f6b6113bb60c6ad7f0d53841b9bbc21e38e
ry05k2ulv <ryosuke.10117@gmail.com>
2023-09-19 15:56:29 +0900

    update template

--- atcoder/template.kt
+++ atcoder/template.kt
@@ -4,5 +4,9 @@ private fun readLong() = readStr().toLong() // single long
 private fun readStrings() = readStr().split(" ") // list of strings
 private fun readInts() = readStrings().map { it.toInt() } // list of ints
 private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun <A, B> readPair(first: (String) -> A, second: (String) -> B) =
+	with(readStrings()) { first(get(0)) to second(get(1)) } // single pair
+private fun <A, B, C> readTriple(first: (String) -> A, second: (String) -> B, third: (String) -> C) =
+	with(readStrings()) { Triple(first(get(0)), second(get(1)), third(get(2))) } // single triple
 
 fun main() {}
